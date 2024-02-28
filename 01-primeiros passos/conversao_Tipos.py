3eb20b15c8ba9f8340797da6c3992a56161d48dc
Marcos Montezi <marcosrincao@yahoo.com.br>
2024-02-28 17:38:45 -0300

    Novas inclusões no aprendizado de Python

---
+++ 01-primeiros passos/conversao_Tipos.py
@@ -0,0 +1,10 @@
+preco = 10.50
+idade = 30
+
+# Conversão de float para string
+print(str(preco))
+print(str(idade))
+
+texto = f"Idade:{idade}, preço:{preco}"
+
+print(texto)
---
+++ 01-primeiros passos/usando_Input.py
@@ -0,0 +1,7 @@
+nome = input("Informe seu nome: ")
+idade = input("Infome sua idade: ")
+
+print("Primeira Fase com Python 1.0")
+print("##############")
+print(f'Seu nome é {nome} e sua idade é {idade}', end="...\n")
+print(nome, idade, sep=" - ")

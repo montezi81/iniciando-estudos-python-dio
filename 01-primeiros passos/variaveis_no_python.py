79ace11e62ae5d37835d3447673c3d6db019dd11
Marcos Montezi <marcosrincao@yahoo.com.br>
2024-02-28 17:03:44 -0300

    Primeiros passos com python 1.0

---
+++ 01-primeiros passos/variaveis_no_python.py
@@ -0,0 +1,7 @@
+idade = 25
+nome_Cliente = 'Marcos'
+# Padrão de constante (utilizar a variável em caixa alta VALOR_DE_PI = 3.14)
+
+print(f'O nome do cliente é {nome_Cliente} e sua idade é {idade}')
+
+

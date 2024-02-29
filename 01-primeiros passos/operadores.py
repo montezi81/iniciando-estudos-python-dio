c13a54646f63f7dc68e89fa700c88c540d57c489
Marcos Montezi <marcosrincao@yahoo.com.br>
2024-02-29 09:00:52 -0300

    Operadores Básicos no Python

---
+++ 01-primeiros passos/operadores.py
@@ -0,0 +1,8 @@
+valor_1 = 5
+valor_2 = 10
+
+print(valor_1 ** valor_2)
+print(valor_1 // valor_2)
+print(valor_1 * valor_2 - valor_1)
+print(valor_1 * valor_2 * valor_2)
+print(valor_1 * valor_2 + valor_1)
